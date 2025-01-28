9e968e2f5a7a575d0964e761005e341ec58d2a0c
Vitor <v.marrahernandes@gmail.com>
2025-01-28 19:51:20 -0300

    URL de download bindado ao user input

--- scripts.js
+++ scripts.js
@@ -3,21 +3,19 @@ const fs = require("fs");
 const path = require("path");
 
 function download() {
-    const url = "https://www.youtube.com/watch?v=LBbdyF16qKc";
+    let userInput = document.getElementById("user-input");
+    let url = userInput.value;
 
-    // Caminho para a área de trabalho do usuário
-    const desktopPath = path.join(process.env.USERPROFILE || process.env.HOME, "Desktop");
-    const outputPath = path.join(desktopPath, "video.mp4");
+    let desktopPath = path.join(process.env.USERPROFILE || process.env.HOME, "Desktop");
+    let outputPath = path.join(desktopPath, "video.mp4");
 
     try {
-        const stream = ytdl(url).pipe(fs.createWriteStream(outputPath));
+        let stream = ytdl(url).pipe(fs.createWriteStream(outputPath));
 
-        // Mensagem de sucesso ao finalizar
         stream.on("finish", () => {
             console.log("Download finalizado com sucesso! O vídeo foi salvo na área de trabalho como:", outputPath);
         });
 
-        // Mensagem de erro durante o processo
         stream.on("error", (err) => {
             console.error("Ocorreu um erro durante o download:", err.message);
         });
