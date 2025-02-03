fb08d026f893e1187418afeb8bff002774d15514
Vitor <v.marrahernandes@gmail.com>
2025-02-03 19:11:35 -0300

    Atualização do ytdl e umas coisinhas

--- backend.js
+++ backend.js
@@ -3,13 +3,20 @@ const fs = require("fs");
 const path = require("path");
 
 function download() {
-    let url = ""
+    let url = "https://www.youtube.com/watch?v=eKAhu7DyZN8"
 
     let desktopPath = path.join(process.env.USERPROFILE || process.env.HOME, "Desktop");
     let outputPath = path.join(desktopPath, "video.mp4");
 
     try {
         let stream = ytdl(url).pipe(fs.createWriteStream(outputPath));
+        stream.on("open", () => {
+            console.log("Iniciando download...")
+        });
+
+        stream.on("data", () => {
+            console.log("Baixando...")
+        });
 
         stream.on("finish", () => {
             console.log("Download finalizado com sucesso! O vídeo foi salvo na área de trabalho como:", outputPath);
